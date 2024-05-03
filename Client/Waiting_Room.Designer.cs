5be23542b940300830bec0df2655679d6e476886
wuoc <quockhanh4114@gmail.com>
2024-05-03 23:33:04 +0700

    add image

--- Client/Waiting_Room.Designer.cs
+++ Client/Waiting_Room.Designer.cs
@@ -132,7 +132,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(538, 289);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button_play);
